78661b487aa2d31395c1100a587f104c24f3017d
Mansanga <150928510+mansProjects@users.noreply.github.com>
2024-01-26 19:00:17 +0100

    Delete Happy_New_year directory

--- Happy_New_year/src/js/index.js
+++
@@ -1,7 +0,0 @@
-const incredible = document.querySelector('.incredible')
-const main = document.querySelector('main .main')
-
-setTimeout(() => {
-    incredible.classList.add('none')
-    main.classList.add('incredible')
-}, 20000);
